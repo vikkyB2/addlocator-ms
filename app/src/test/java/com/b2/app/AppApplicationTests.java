d476d23ab8e7f92aa08bec87b4a3f6be173e9c12
vikkyB2 <vigneshs21225@gmail.com>
2023-03-11 16:44:57 +0530

    Initial commit

---
+++ app/src/test/java/com/b2/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.b2.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
